57ba9e7025449c8eb016a177c5d6a8765e24b2f8
Diana <dianaserik0@gmail.com>
2023-12-21 18:17:09 +0500

    добавлен линтер

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 import readlineSync from 'readline-sync';
+
 const rockPaperScissors = (decitionPlayer) => {
-    decitionPlayer = decitionPlayer.toLowerCase();
-    const randomDecition = Math.random();
-    let decitionComp;
-    if (randomDecition < 0.33) {
-        decitionComp = "камень";
-    } else if (randomDecition < 0.66) {
-        decitionComp = "бумага";
-    } else {
-        decitionComp = "ножницы"
-    }
-    if (decitionPlayer === decitionComp) {
-        return "У вас с компьютером одинковый выбор - Ничья";
-    }
-    if (decitionPlayer === "камень" && decitionComp === "ножницы") {
-        return `Ваш выбор: ${decitionPlayer} 
+  decitionPlayer = decitionPlayer.toLowerCase();
+  const randomDecition = Math.random();
+  let decitionComp;
+  if (randomDecition < 0.33) {
+    decitionComp = 'камень';
+  } else if (randomDecition < 0.66) {
+    decitionComp = 'бумага';
+  } else {
+    decitionComp = 'ножницы';
+  }
+  if (decitionPlayer === decitionComp) {
+    return 'У вас с компьютером одинковый выбор - Ничья';
+  }
+  if (decitionPlayer === 'камень' && decitionComp === 'ножницы') {
+    return `Ваш выбор: ${decitionPlayer} 
 Выбор компьютера: ${decitionComp}
 Игрок: победил`;
-    } else if (decitionPlayer === "бумага" && decitionComp === "камень") {
-        return `Ваш выбор: ${decitionPlayer} 
+  } if (decitionPlayer === 'бумага' && decitionComp === 'камень') {
+    return `Ваш выбор: ${decitionPlayer} 
 Выбор компьютера: ${decitionComp}
 Игрок: победил`;
-    } else if (decitionPlayer === "ножницы" && decitionComp === "бумага") {
-        return `Ваш выбор: ${decitionPlayer} 
+  } if (decitionPlayer === 'ножницы' && decitionComp === 'бумага') {
+    return `Ваш выбор: ${decitionPlayer} 
 Выбор компьютера: ${decitionComp}
 Игрок: победил`;
-    } else {
-        return `Ваш выбор: ${decitionPlayer} 
+  }
+  return `Ваш выбор: ${decitionPlayer} 
 Выбор компьютера: ${decitionComp}
 Игрок: проиграл`;
-    }   
-}
+};
 
 console.log(rockPaperScissors(readlineSync.question('Ваш выбор ')));
