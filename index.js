e6729a6607e00197754d091c102a1b6ac32e3438
Diana <dianaserik0@gmail.com>
2023-12-21 13:48:15 +0500

    lll

---
+++ index.js
@@ -0,0 +1,27 @@
+import readlineSync from 'readline-sync';
+const rockPaperScissors = (decitionPlayer) => {
+    decitionPlayer = decitionPlayer.toLowerCase();
+    const randomDecition = Math.random();
+    let decitionComp;
+    if (randomDecition < 0.33) {
+        decitionComp = "камень";
+    } else if (randomDecition < 0.66) {
+        decitionComp = "бумага";
+    } else {
+        decitionComp = "ножницы"
+    }
+    if (decitionPlayer === decitionComp) {
+        return "У вас с компьютером одинковый выбор - Ничья";
+    }
+    if (decitionPlayer === "камень" && decitionComp === "ножницы", decitionPlayer === "бумага" && decitionComp === "камень", decitionPlayer === "ножницы" && decitionComp === "бумага") {
+        return `Ваш выбор: ${decitionPlayer} 
+Выбор компьютера: ${decitionComp}
+Игрок: победил`;
+    } else {
+        return `Ваш выбор: ${decitionPlayer} 
+Выбор компьютера: ${decitionComp}
+Игрок: проиграл`;
+    }   
+}
+
+console.log(rockPaperScissors(readlineSync.question('Ваш выбор ')));
